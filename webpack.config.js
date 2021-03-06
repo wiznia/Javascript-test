224a1fb5eec0debf29292fb3719f25cd60041ab6
Ariel Wiznia <wizniaa@amazon.com>
2021-03-06 16:37:45 -0300

    Initial commit, adding git repo

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [new HtmlWebpackPlugin(), new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js'],
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+};
