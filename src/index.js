319f80e2401d7e8bef2605ebe3230276b6a5a97e
Ariel Wiznia <wizniaa@amazon.com>
2021-03-07 19:01:01 -0300

    Added gitignore, added getLocationInfo, fetchDoctors and updateDoctors functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
 import "./index.css";
+// Had to import this module so I could use async/await with Webpack
+import 'regenerator-runtime/runtime';
+
+async function getLocationInfo() {
+  // We first get the static HTML so we can insert its location properties to the API
+  Array.from(document.querySelectorAll('[data-upin]')).forEach(doctor => {
+    const upin = doctor.dataset.upin;
+    const zipCode = parseInt(doctor.querySelectorAll('td')[1].innerHTML);
+    const city = doctor.querySelectorAll('td')[2].innerHTML;
+    const location = {
+      zipCode,
+      city
+    };
+
+    updateDoctors(upin, location);
+  });
+}
+
+async function fetchDoctors(param) {
+  const params = param ? `?${param}=true` : '';
+  const response = await fetch(`${process.env.API}/doctors${params}`);
+  response.json().then(data => {
+    // Rebuild tables once the data arrives
+  }).catch(error => {
+    console.error('Error:', error);
+  });
+};
+
+async function updateDoctors(upin, data) {
+  const options = {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  };
+
+  await fetch(`${process.env.API}/doctors/${upin}`, options)
+    .then(response => { return response.json() })
+    .then(data => {
+      console.log('Successfully updated API info', data);
+    }).catch(error => {
+      console.error('Error:', error);
+    });
+};
+
+async function getData() {
+  await getLocationInfo();
+  fetchDoctors();
+};
+
+getData();
